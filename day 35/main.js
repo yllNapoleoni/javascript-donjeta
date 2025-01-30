2b2c05501794a05a6fdfd89731b9b60e1684ba8a
yllNapoleoni <166026966+yllNapoleoni@users.noreply.github.com>
2025-01-30 20:42:35 +0100

    day 35

---
+++ day 35/main.js
@@ -0,0 +1,10 @@
+function openmenu(){
+    var menu =document.getElementById("mylinks");
+
+    if(menu.style.display==="block"){
+        menu.style.display="none"
+
+    }else{
+        menu.style.display="block"
+    }
+}
