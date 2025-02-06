c4267c0711680bca6ff901e10c24f6eff8d69a88
yllNapoleoni <166026966+yllNapoleoni@users.noreply.github.com>
2025-02-06 20:14:25 +0100

    dita 36

--- day 36/main.js
+++ day 36/main.js
@@ -16,8 +16,26 @@ $('#btn2').on("click", function(){
 $('#mydiv').click(function(){
     $(this).addClass("clicked");
 })
+.find('span')
+.attr('title','hover over me');
 
-$('btn3').click(function(){
-    
+$('#btn3').click(function(){
+    $(".hidden").show('slow');
+});
+$("#btn4").click(function(){
+    $(".hidden").hide();
+});
+
+$(".testing").click(function(){
+    $(".testing").animate({
+        'left':'+=50px',
+        'opacity':0.25,
+        'fontSize':'30px'
+    },300,
+    function(){
+        console.log("animation is completed")
+    }
+
+);
 })
 
